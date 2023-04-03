0b1319c3fbc1282dc020841491d984b1232b7ad5
michael <kurashevmichael@gmail.com>
2023-04-03 17:24:21 +0300

    lesson_10

--- client.py
+++ client.py
@@ -1,30 +1,62 @@
-from utils import init_socket_tcp, serialization_message, deserialization_message, sys_param_reboot
+from utils import init_socket_tcp, serialization_message, sys_param_reboot, deserialization_message_list
 from decorators import log
-import datetime, logging, sys, json, threading, time
+import datetime, logging, sys, json, threading, time, socket
 from log import client_log_config
+from metaclasses import ClientVerifier
 
 app_log_client = logging.getLogger('client')
 
 
-@log
-def create_message(message, user_name):
-    """Создаем сообщение для отправки на сервер."""
-    msg = {
-        "action": 'presence',
-        'time': datetime.datetime.now().strftime('%d.%m.%Y'),
-        'user': {
-            'account_name': user_name,
-        },
-        'mess_text': message
-    }
-    app_log_client.info('Сообщение создано')
-    return msg
+class ClientSender(threading.Thread, metaclass=ClientVerifier):
+    def __init__(self, account_name, sock):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
+
+    def send_message(self):
+        while True:
+            message = input()
+            msg = {
+                "action": 'presence',
+                'time': datetime.datetime.now().strftime('%d.%m.%Y'),
+                'user': {
+                    'account_name': self.account_name,
+                },
+                'mess_text': message
+            }
+            app_log_client.info('Сообщение создано')
+            byte_msg = serialization_message(msg)
+            app_log_client.info('Сообщение сериализовано')
+            self.sock.send(byte_msg)
+            time.sleep(1)
+
+    def run(self):
+        self.send_message()
+
+
+class ClientRecipient(threading.Thread, metaclass=ClientVerifier):
+    def __init__(self, sock):
+        self.sock = sock
+        super().__init__()
+
+    def get_message(self):
+        while True:
+            data = self.sock.recv(1024)
+            list_message = deserialization_message_list(data)
+            for i in list_message:
+                app_log_client.info('Сообщение десериализовано')
+                app_log_client.info('Ответ получен. %s %s', i['response'], i['alert'])
+                print(f"{i['user_name']} >> {i['message']}")
+            time.sleep(1)
+
+    def run(self):
+        self.get_message()
 
 
 def install_param_in_socket_client():
     """Устанавливаем введенные пользователем параметры подключения к серверу/создания сервера"""
     param = sys.argv
-    port = 10003
+    port = 10005
     addr = 'localhost'
     try:
         for i in param:
@@ -41,63 +73,31 @@ def install_param_in_socket_client():
         return error, name_error
 
 
-def decode_message(message):
-    dec_mes = message.decode('utf-8')
-    return dec_mes
-
-
-def replace_data_message(decode_mes):
-    replace_data = decode_mes.replace('}{', '} , {')
-    return replace_data
-
-
-def split_message(replace_mes):
-    split_data = replace_mes.split(' , ')
-    return split_data
-
-
-def deserialization_message_list(message):
-    list_deserialization_message = []
-    d_mes = decode_message(message)
-    r_mes = replace_data_message(d_mes)
-    s_mes = split_message(r_mes)
-    for i in s_mes:
-        list_deserialization_message.append(json.loads(i))
-    return list_deserialization_message
-
-
-def send_message(serv, name):
-    while True:
-        msg = create_message(input(''), name)
-        byte_msg = serialization_message(msg)
-        app_log_client.info('Сообщение сериализовано')
-        serv.send(byte_msg)
-        time.sleep(1)
-
-
-def get_message(serv):
-    while True:
-        data = serv.recv(1024)
-        list_message = deserialization_message_list(data)
-        for i in list_message:
-            app_log_client.info('Сообщение десериализовано')
-            app_log_client.info('Ответ получен. %s %s', i['response'], i['alert'])
-            print(f"{i['user_name']} >> {i['message']}")
-        time.sleep(1)
-
-
 def main():
+    # инициализируем сокет
     server = init_socket_tcp()
+    print('Консольный мессенджер')
     app_log_client.info('Сокет инициализирован')
+
+    # берем параметры для подключения к серверу, переданные при запуске сервера
     addr, port = install_param_in_socket_client()
+
+    # подключаемся к серверу
     server.connect((addr, port))
+    app_log_client.info(f'Установлено соединение с сервером. Ответ сервера: 200')
 
-    t_send_message = threading.Thread(target=send_message, args=(server, server.getsockname()[1]), name='theard-1')
-    t_send_message.start()
+    # создаем имя пользователя
+    name_accaunt = input('Введите имя пользователя: ')
 
-    t_get_message = threading.Thread(target=get_message, args=(server,), name='theard-2')
-    t_get_message.start()
+    # создаем поток отправки сообщения от сервера
+    client_sender = ClientSender(name_accaunt, server)
+    client_sender.start()
+
+    # создаем поток принятия сообщения от сервера
+    client_recipient = ClientRecipient(server)
+    client_recipient.start()
 
 
+# вызываем функцию main() при запуске кода
 if __name__ == '__main__':
     main()
